0351e8394421115e2550d150b0cd4ae8059b9f6b
watermelonhater <watermelonhater33@gmail.com>
2025-04-27 21:13:24 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu');
+
+
+menuBtn.addEventListener('click', ()=> {
+    menu.classList.toggle('menu--open')
+});
